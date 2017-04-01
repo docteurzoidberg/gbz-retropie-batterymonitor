ebe421c9fb30dd349ea2924924643088dd68b905
DrZoid <drzoid@tppt.eu>
2017-04-01 15:09:14 +0200

    compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -o gbz-rpi-battmon debug-Lib/src/Logger.cpp serial-Lib/src/Serial.cpp gbz-rpi-battmon.cpp
+g++ -std=c++17 -g3 -Wall -Wextra -Wno-pmf-conversions -o gbz-rpi-battmon debug-Lib/src/Logger.cpp serial-Lib/src/Serial.cpp gbz-rpi-battmon.cpp
